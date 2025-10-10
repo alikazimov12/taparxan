e32334843d8431071c24a338ba3795d6771d3437
alikazimov12 <alikazimov4.25@gmail.com>
2025-10-10 18:00:50 +0400

    alsdj

--- alindanolar.py
+++ alindanolar.py
@@ -12,8 +12,20 @@ dp = Dispatcher()
 BASE_URL = "https://tap.az/elanlar/elektronika/noutbuklar"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0",
-    "Accept": "text/html, */*; q=0.01",
-    "X-Requested-With": "XMLHttpRequest"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Dnt": "1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Cookie": "csrf_debug_at=2025-10-07+21%3A59%3A56+%2B0400; _tapaz_session=..."  # öz cookie-ni buraya yapıştır
 }
 
 subscribers_file = "subscribers.json"
